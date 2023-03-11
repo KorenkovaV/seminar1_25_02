cbe2a5fbfef249a3c730f3ab1b51d43cb7bb99d8
Vera <vv.korenkova@yandex.ru>
2023-03-11 18:37:59 +0300

    add 2_1 my vers

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// в конце после объявления функции ; не ставится
+
+int big(int rand)
+{
+    Console.WriteLine(rand);
+    int a = (rand - rand % 10) / 10;
+    int b = rand % 10;
+    if (a > b) return a;
+    else return b;
+}
+int result = big(new Random().Next(10, 100));
+Console.WriteLine(result);
