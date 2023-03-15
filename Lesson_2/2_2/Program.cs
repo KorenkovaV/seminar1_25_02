05e7daf794cc8a0bcf1d1b140bc6e82599050b7e
Vera <vv.korenkova@yandex.ru>
2023-03-15 21:58:43 +0300

    add 2_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// 3. Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деление.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+void mult(int a, int b)
+{
+    if (a % b == 0)
+        Console.WriteLine("multiple");
+    else
+        Console.WriteLine($"not multiple, remainder of division is {a % b}");
+}
+
+Console.WriteLine("Enter number a");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+mult(a, b);
