60cdd11e08f92ac093ba4ebc457b492a460db90d
Vera <vv.korenkova@yandex.ru>
2023-03-15 22:51:09 +0300

    add hw2 ex2

---
+++ Lesson_2/Homework_2/ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 13: Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+void del(int num)
+{
+    Console.WriteLine(num);
+    int a = num / 100;
+    int b = num % 10;
+    Console.WriteLine($"{a}{b}");
+}
+
+int num = new Random().Next(100, 1000);
+
+del(num);
